08e1efd40d7de2f88488ce014b81e6eb4fc76d82
Bendegúz Tóth <toth.bendeguz@students.jedlik.eu>
2025-12-02 08:36:48 +0100

    Alap projekt struktúra

---
+++ Könyvtár/Könyvtár/App.xaml.cs
@@ -0,0 +1,8 @@
+﻿using System.Windows;
+
+namespace ModernLibrary
+{
+    public partial class App : Application
+    {
+    }
+}
---
+++ Könyvtár/Könyvtár/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace ModernLibrary
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
